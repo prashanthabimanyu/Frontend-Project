a1f6ba12776c6e943ac685a29d7445865e7958a4
prashanthabimanyu <121157434+prashanthabimanyu@users.noreply.github.com>
2023-04-02 13:24:46 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# E-Commerce_App
+This ecommerce store was built using React JS and React Redux, and leverages fakestoreapi.com for product data. It allows users to add items to their cart, view product descriptions, adjust quantities, and see the total price. This project is made with React JS and Redux.
+
+
+Website link : https://ecommerceappfakeapi.netlify.app
---
+++ src/controllers/redux_store/action/productsAction.jsx
@@ -0,0 +1,33 @@
+import { ActionTypes } from "../constants/actionType";
+const {
+  UPDATE_PRODUCT_COUNTER,
+  SET_CHECKOUT,
+  INCREMENT,
+  DECREMENT,
+  SET_PRODUCTS,
+} = ActionTypes;
+
+export function increment(id) {
+  const payload = {
+    id: id,
+    updateType: INCREMENT,
+  };
+ 
+  return { type: UPDATE_PRODUCT_COUNTER, payload };
+}
+export function decrement(id) {
+  const payload = {
+    id: id,
+    updateType: DECREMENT,
+  };
+ 
+  return { type: UPDATE_PRODUCT_COUNTER, payload };
+}
+
+export function setProducts(products) {
+  return { type: SET_PRODUCTS, payload: products };
+}
+export function checkout() {
+  console.log("This function have called");
+  return { type: SET_CHECKOUT };
+}
